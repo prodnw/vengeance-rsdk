8c4b08f89df3fbc68e79ff62e0ffd8100f264d36
ProdNW <79802429+prodnw@users.noreply.github.com>
2024-06-30 00:15:48 +0100

    testing...

--- RSDKv4/Debug.cpp
+++ RSDKv4/Debug.cpp
@@ -503,11 +503,10 @@ void SetTextMenu(int sm)
     SetupTextMenu(&gameMenu[1], 0);
     switch (sm) {
         case DEVMENU_MAIN: {
-            AddTextMenuEntry(&gameMenu[0], "RSDK DEV MENU");
+            AddTextMenuEntry(&gameMenu[0], "RSDKv4 DEV MENU");
             AddTextMenuEntry(&gameMenu[0], " ");
             char version[0x80];
             StrCopy(version, Engine.gameWindowText);
-            StrAdd(version, " Version");
             AddTextMenuEntry(&gameMenu[0], version);
             AddTextMenuEntry(&gameMenu[0], Engine.gameVersion);
 #ifdef RETRO_DEV_EXTRA
--- RSDKv4/RetroEngine.hpp
+++ RSDKv4/RetroEngine.hpp
@@ -444,7 +444,7 @@ public:
 #ifdef DECOMP_VERSION
     const char *gameVersion = DECOMP_VERSION;
 #else
-    const char *gameVersion  = "v4";
+    const char *gameVersion  = "";
 #endif
     const char *gamePlatform = nullptr;
 
